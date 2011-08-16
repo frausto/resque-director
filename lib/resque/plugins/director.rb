d9489057d4e9044f316ba5e038f1d7e2b3ddee0f
Nolan <nrfrausto@gmail.com>
2011-08-16 01:00:43 -0400

    refactor the director slightly

--- lib/resque/plugins/director.rb
+++ lib/resque/plugins/director.rb
@@ -14,6 +14,8 @@ module Resque
       end
       
       def before_perform_direct_workers(*args)
+        return unless scaling_config_set?
+        
         time_through_queue = Time.now - (@start_time || Time.now)
         jobs_in_queue = Resque.size(@queue.to_s)
         
@@ -26,6 +28,10 @@ module Resque
       
       private
       
+      def scaling_config_set?
+        Config.max_time > 0 || Config.max_queue > 0
+      end
+      
       def scale_up?(time_through_queue, jobs_in_queue)
         time_limits_exceeded =  Config.max_time > 0 && time_through_queue > Config.max_time 
         queue_limits_exceeded = Config.max_queue > 0 && jobs_in_queue > Config.max_queue
@@ -35,7 +41,7 @@ module Resque
       def scale_down?(time_through_queue, jobs_in_queue)
         time_limits =  Config.max_time > 0 && time_through_queue < (Config.max_time/2)
         queue_limits = Config.max_queue > 0 && jobs_in_queue < (Config.max_queue/2) 
-        (Config.max_time > 0 || Config.max_queue > 0) && (Config.max_time <= 0 || time_limits) && (Config.max_queue <= 0 || queue_limits)
+        (Config.max_time <= 0 || time_limits) && (Config.max_queue <= 0 || queue_limits)
       end
     end
   end
